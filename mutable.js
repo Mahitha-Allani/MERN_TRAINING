039c10e4b4fff82114c11c2488c763b7336ef124
Mahitha <allanimahitha@gmail.com>
2026-01-06 14:55:47 +0530

    Keep only 3 assignment files

--- mutable.js
+++
@@ -1,6 +0,0 @@
-//immutable.js
-let a=10
-a=a+1;
-console.log(a);
-a=a*10
-console.log(a);
--- task4.js
+++
@@ -1,8 +0,0 @@
-// create product obj with properties productName,brand,price
-
-let product = {
-    productName: "Laptop",
-    brand: "Dell",
-    price: 65000
-};
-console.log(product);
