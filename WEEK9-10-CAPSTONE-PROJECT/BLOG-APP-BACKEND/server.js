76cead6cdb3cec9e26416591e1df04e1f79ed948
Mahitha <allanimahitha@gmail.com>
2026-04-01 19:42:45 +0530

    fixed API base URL

--- WEEK9-10-CAPSTONE-PROJECT/BLOG-APP-BACKEND/server.js
+++ WEEK9-10-CAPSTONE-PROJECT/BLOG-APP-BACKEND/server.js
@@ -17,7 +17,7 @@ const app = exp();
 // enable CORS
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://mern-training-lemon.vercel.app"],
     credentials: true
   })
 );
